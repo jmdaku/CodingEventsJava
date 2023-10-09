0986c5274f71e07d52f7948041b6f782c23e4fad
jmdaku <jmdaku@gmail.com>
2023-10-08 23:21:12 -0400

    added validation & event type dropdown

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,8 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -7,14 +10,30 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message ="Name is required.")
+    @Size(min = 3, max =50, message = "Name must be more than 3 characters and less than 50 characters.")
     private String name;
+
+    @Size(max =100, message ="Description too long.")
     private String description;
 
-    public Event(String name, String description) {
-        this.name = name;
-        this.description = description;
+    @NotBlank(message ="Contact email is required.")
+    @Email(message = "Not a valid email.")
+    private String contactEmail;
+
+    private EventType type;
+
+    public Event() {
         this.id = nextId;
         nextId++;
+    }
+
+    public Event(String name, String description, String contactEmail, EventType type) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
     }
 
     public int getId() {
@@ -36,6 +55,22 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
